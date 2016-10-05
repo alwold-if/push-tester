f4e266946df29a6ec74851ca5ba55cef758648a5
Al Wold <albert.wold@ifactorinc.com>
2016-10-04 23:16:32 -0700

    add fold command

--- convert-authkey.sh
+++ convert-authkey.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-openssl pkcs8 -nocrypt -in $1 -outform DER -out $2 -topk8
+# clean up the PEM formatting from apple
+fold -w 64 $1 > $1.folded
+
+openssl pkcs8 -nocrypt -in $1.folded -outform DER -out $2 -topk8
